f7ed987a1e3c3e336d7596b8cfafabc073843141
Fahmi <mumi080302@gmail.com>
2025-03-24 06:14:42 +0700

    Update tombol kembali keatas

--- script.js
+++ script.js
@@ -32,4 +32,17 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
-​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​
+​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​// Tombol Kembali ke Atas
+window.onscroll = function() {
+    let backToTopButton = document.getElementById('back-to-top');
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopButton.style.display = 'block';
+    } else {
+        backToTopButton.style.display = 'none';
+    }
+};
+
+document.getElementById('back-to-top').addEventListener('click', function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
